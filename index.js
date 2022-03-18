50c4717b5ce8e892d0d57b38d1afb18e35e38cae
Leo Liao <honglin0822@outlook.com>
2022-03-18 08:29:54 +0800

    adjust

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const luxon = require("luxon");
 const axios = require("axios");
 const app = express();
 const port = 7777;
-
 const token = "{Any token you want to valid}";
 const discord_webhookUrl = "{Discord Webhook Url}";
+const timeZone = "{Any Zone as Asia/Shanghai}";
 
 app.use(bodyParser.json());
 
@@ -15,28 +15,35 @@ app.get("/", (req, res) => {
 });
 
 app.post("/pr", (req, res) => {
-  // check token is equals
+  const time = luxon.DateTime.now()
+    .setZone(timeZone)
+    .toFormat("yyyy/MM/dd HH:mm:ss");
+
+  console.log(`${time} - Get Integration!`);
+
   if (req.headers["x-gitlab-token"] !== token) {
+    console.log(`${time} - Not Validate!`);
     return res.status(404);
   }
 
   const state = req.body.object_attributes.state;
+  const state_list = ["opened", "update"];
 
-  // only capture pull request open or update
-  const state_list = ["open", "update"];
+  console.log(`${time} - From Type ${state}`);
 
   if (state_list.includes(state)) {
     const author = req.body.user.name;
     const email = req.body.user.email;
     const title = req.body.object_attributes.title;
-    const description = req.body.object_attributes.description;
+    const description = req.body.object_attributes.description
+      ? req.body.object_attributes.description
+      : "-";
     const source_branch = req.body.object_attributes.source_branch;
     const url = req.body.object_attributes.url;
     const create_time = luxon.DateTime.fromISO(
       req.body.object_attributes.created_at
     )
-      // set your area zone
-      .setZone("Asia/Shanghai")
+      .setZone(timeZone)
       .toFormat("yyyy/MM/dd HH:mm:ss");
 
     const json = {
@@ -74,7 +81,11 @@ app.post("/pr", (req, res) => {
       ],
     };
 
-    axios.post(discord_webhookUrl, json);
+    console.log(`${time} - Data ${JSON.stringify(json)}`);
+
+    axios.post(discord_webhookUrl, json).catch((error) => {
+      console.log(`${time} - Error ${error}`);
+    });
   }
 
   res.send("Success!");
